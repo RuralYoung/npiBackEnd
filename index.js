90fac2e9564a650cd2d37a3441d19a672aee7068
RuralYoung <ruralyoungiii@gmail.com>
2023-04-27 14:15:15 -0400

    Properly set up a GET to retreive npi data api 'get'

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-const npiData = require('./npiData.js');
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.json())
 
-app.use('/npiData', npiData);
+app.get('/', (req, res) => {
+    console.log("Received GET Request")
 
-app.listen(5000)
+    fetch(`https://npiregistry.cms.hhs.gov/api/?version=2.1&city=Atlanta&limit=50&pretty=on`)
+    .then(response => response.json())
+    .then(data => {
+        return res.send(data);
+    })
+})
+
+http.createServer(app).listen(process.env.PORT || 5000)
