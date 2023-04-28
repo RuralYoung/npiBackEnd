e471f816db1515a8b34756d32b9e73e8bac832d0
RuralYoung <ruralyoungiii@gmail.com>
2023-04-28 00:57:16 -0400

    Added the query-able stuff to the get node
    Now the API should send back anything the front end sends in.

--- index.js
+++ index.js
@@ -11,14 +11,19 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     console.log("Received GET Request")
-    let city = req.query.city;
     let skip = req.query.skip;
+    let firstName = req.query.first_name;
+    let lastName = req.query.last_name;
+    let npiNum = req.query.number;
+    let city = req.query.city;
+    let state = req.query.state;
+    let zip = req.query.postal_code;
 
-    fetch(`https://npiregistry.cms.hhs.gov/api/?version=2.1&city=${city}&limit=200&skip=${skip}&pretty=on`)
-    .then(response => response.json())
-    .then(data => {
-        return res.send(data);
-    })
+    fetch(`https://npiregistry.cms.hhs.gov/api/?version=2.1&first_name=${firstName}&last_name=${lastName}&number=${npiNum}&city=${city}&state=${state}&postal_code=${zip}&limit=200&skip=${skip}&pretty=on`)
+        .then(response => response.json())
+        .then(data => {
+            return res.send(data);
+        })
 })
 
 http.createServer(app).listen(process.env.PORT || 5000)
